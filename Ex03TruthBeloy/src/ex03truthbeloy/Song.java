6e6549dd4342b7a06552b7313e77ca0b1cd9a145
karlbeloy000 <113119033+karlbeloy000@users.noreply.github.com>
2022-09-21 19:37:48 +0800

    Add files via upload

---
+++ Ex03TruthBeloy/src/ex03truthbeloy/Song.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ex03truthbeloy;
+
+/**
+ *
+ * @author Karl
+ */
+public class Song {
+    
+    String title;
+    String artist;
+    
+    public Song(String t, String a) {
+        title = t;
+        artist = a;
+    }
+}
